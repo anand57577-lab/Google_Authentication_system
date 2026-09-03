946d4de82d7faf9d31b396c692573d98451aeac2
S ANAND <anand.57577@gmail.com>
2026-09-03 11:35:14 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -27,7 +27,9 @@ app.add_middleware(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        settings.FRONTEND_URL
+        "https://google-authentication-system.vercel.app",
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
     ],
     allow_credentials=True,
     allow_methods=["*"],
